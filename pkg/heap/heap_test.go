c15b5760df7c978ad45289efc23ac37302ad09a5
DerGut <jannik.steinmann@gmx.de>
2024-03-03 17:15:42 +0100

    Check errno

--- pkg/heap/heap_test.go
+++ pkg/heap/heap_test.go
@@ -10,9 +10,11 @@ func TestHeap(t *testing.T) {
 	h := heap.New("test")
 	defer h.Close()
 
-	h.Set("key", "value")
-	value := h.Get("key")
+	if err := h.Set("key", "value"); err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
 
+	value := h.Get("key")
 	if value != "value" {
 		t.Errorf("expected value to be \"value\", got %q", value)
 	}
