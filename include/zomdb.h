b368d39fd46feb896203b2eacb0db181de25a481
DerGut <jannik.steinmann@gmx.de>
2024-03-27 22:36:17 +0100

    Include static lib and header

---
+++ include/zomdb.h
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define ERR_IO 10
+
+#define ERR_UTF8 30
+
+typedef struct Heap Heap;
+
+struct Heap *create_heap(const char *file_name_cstr);
+
+const char *heap_get(struct Heap *ptr, const char *key_cstr);
+
+void heap_set(struct Heap *ptr, const char *key_cstr, const char *value_cstr);
+
+void destroy_heap(struct Heap *ptr);
