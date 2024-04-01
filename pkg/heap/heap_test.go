8c21f3914ca93ad3d6b81fedb4a82b996bf40bc7
Jannik Steinmann <jannik.steinmann@gmx.de>
2024-04-01 18:01:34 +0200

    :white_check_mark: Add test + refactor (#41)
    * New test for overwrites
    
    Signed-off-by: DerGut <jannik.steinmann@datadoghq.com>
    
    * Refactor heap creation in tests
    
    Signed-off-by: DerGut <jannik.steinmann@datadoghq.com>
    
    ---------
    
    Signed-off-by: DerGut <jannik.steinmann@datadoghq.com>

--- pkg/heap/heap_test.go
+++ pkg/heap/heap_test.go
@@ -9,13 +9,7 @@ import (
 )
 
 func TestHeap(t *testing.T) {
-	name := filepath.Join(t.TempDir(), "test.zomdb")
-
-	h, err := heap.New(name)
-	if err != nil {
-		t.Fatalf("new: expected no error, got %v", err)
-	}
-	defer h.Close()
+	h := newTestHeap(t)
 
 	if err := h.Set("key", "value"); err != nil {
 		t.Fatalf("set: expected no error, got %v", err)
@@ -32,13 +26,7 @@ func TestHeap(t *testing.T) {
 }
 
 func TestHeapSetAndGetMultiple(t *testing.T) {
-	name := filepath.Join(t.TempDir(), "test.zomdb")
-
-	h, err := heap.New(name)
-	if err != nil {
-		t.Fatalf("new: expected no error, got: %v", err)
-	}
-	defer h.Close()
+	h := newTestHeap(t)
 
 	for i := 0; i < 3; i++ {
 		key := fmt.Sprintf("key_%d", i+1)
@@ -61,17 +49,31 @@ func TestHeapSetAndGetMultiple(t *testing.T) {
 			t.Errorf("expected value to be %q, got %q", value, got)
 		}
 	}
+}
 
+func TestHeapSetOverwrite(t *testing.T) {
+	h := newTestHeap(t)
+
+	if err := h.Set("color", "red"); err != nil {
+		t.Fatalf("set color=red: %v", err)
+	}
+
+	if err := h.Set("color", "green"); err != nil {
+		t.Fatalf("set color=green: %v", err)
+	}
+
+	value, err := h.Get("color")
+	if err != nil {
+		t.Fatalf("get color: %v", err)
+	}
+
+	if value != "green" {
+		t.Errorf("want color=green, got: %s", value)
+	}
 }
 
 func FuzzHeapSet(f *testing.F) {
-	name := filepath.Join(f.TempDir(), "test.zomdb")
-
-	h, err := heap.New(name)
-	if err != nil {
-		f.Fatalf("new: expected no error, got %v", err)
-	}
-	defer h.Close()
+	h := newTestHeap(f)
 
 	f.Add("key", "value")
 	f.Fuzz(func(t *testing.T, a string, b string) {
@@ -79,4 +81,17 @@ func FuzzHeapSet(f *testing.F) {
 			return
 		}
 	})
+}
+
+func newTestHeap(t testing.TB) *heap.Heap {
+	name := filepath.Join(t.TempDir(), "test.zomdb")
+
+	h, err := heap.New(name)
+	if err != nil {
+		t.Fatalf("new heap: %v", err)
+	}
+
+	t.Cleanup(h.Close)
+
+	return h
 }
